13607f163688bf594732293e407bd88dab694171
Arbeen <arbinbabuthapamagar2002@gmail.com>
2026-05-12 20:31:08 +0545

    Implement user registration handler and add token generation utility

---
+++ backend/src/controller/user/user.controller.js
@@ -0,0 +1,27 @@
+import { Admin } from '../models/admin.model.js';
+import { apiError } from '../utils/apiError.js';
+import { apiResponse } from '../utils/apiResponse.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
+import jwt from 'jsonwebtoken';
+
+
+// helper 
+
+
+
+// user login handeling 
+
+
+ const userRegistration = asyncHandler(async()=>{
+
+    const {name,phone, email, password, confirmPassword, avatarUrl, dateOfBirth, gender} = req.body ;
+
+    if (! name || ! phone || ! email || !password || !gender
+     ){
+        throw new apiError( 400, " all field are required ")
+     }
+
+     
+
+
+ })
---
+++ backend/src/utils/generateaccesstokenandrefreshtoke.js
@@ -0,0 +1,16 @@
+
+const generateAccessTokenAndRefreshToken = async (adminId) => {
+  try {
+    const admin = await Admin.findById(adminId);
+    const accessToken = admin.generateAccessToken();
+    const refreshToken = admin.generateRefreshToken();
+    admin.refreshToken = refreshToken;
+    await admin.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new apiError(
+      500,
+      'something went wrong while genereting the accessToken and refreshToken ',
+    );
+  }
+};
